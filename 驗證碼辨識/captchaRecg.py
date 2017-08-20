a09f367e23b6b19bedee26baffb1b8d5d863d764
Arthur <arthur@hopenglish.com>
2017-08-20 16:33:55 +0800

    修正

--- 驗證碼辨識/captchaRecg.py
+++ 驗證碼辨識/captchaRecg.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import cv2
 import numpy as np
 from cv2 import waitKey, matchTemplate, threshold
@@ -9,8 +10,9 @@ import time
 
 tStart = time.time()
 
-im = cv2.imread('test1.jpg', flags=cv2.CV_LOAD_IMAGE_GRAYSCALE)
+im = cv2.imread('test2.jpg', flags=cv2.CV_LOAD_IMAGE_GRAYSCALE)
 retval, im = cv2.threshold(im, 115, 255, cv2.THRESH_BINARY_INV)
+
 for i in xrange(len(im)):
     for j in xrange(len(im[i])):
         if im[i][j] == 255:
@@ -35,14 +37,13 @@ arr = []
 
 for index, (c, _) in enumerate(cnts):
     (x, y, w, h) = cv2.boundingRect(c)
-    roi = im[y: y + h, x: x + w]
-    thresh = roi.copy() 
-    try:
 
+    try:
+        # 只將寬高大於 8 視為數字留存
         if w > 8 and h > 8:
             add = True
             for i in range(0, len(arr)):
-
+                # 這邊是要防止如 0、9 等，可能會偵測出兩個點，當兩點過於接近需忽略
                 if abs(cnts[index][1] - arr[i][0]) <= 3:
                     add = False
                     break
@@ -50,7 +51,40 @@ for index, (c, _) in enumerate(cnts):
                 arr.append((x, y, w, h))
     except IndexError:
         pass
+
+
+for index, (x, y, w, h) in enumerate(arr):
+    roi = im[y: y + h, x: x + w]
+    thresh = roi.copy() 
+    
+    angle = 0
+    smallest = 999
+    row, col = thresh.shape
+
+    for ang in range(-60, 61):
+        M = cv2.getRotationMatrix2D((col / 2, row / 2), ang, 1)
+        t = cv2.warpAffine(thresh.copy(), M, (col, row))
+
+        r, c = t.shape
+        right = 0
+        left = 999
+
+        for i in xrange(r):
+            for j in xrange(c):
+                if t[i][j] == 255 and left > j:
+                    left = j
+                if t[i][j] == 255 and right < j:
+                    right = j
+
+        if abs(right - left) <= smallest:
+            smallest = abs(right - left)
+            angle = ang
+
+    M = cv2.getRotationMatrix2D((col / 2, row / 2), angle, 1)
+    thresh = cv2.warpAffine(thresh, M, (col, row))
+    # resize 成相同大小以利後續辨識
     thresh = cv2.resize(thresh, (50, 50))
+
     cv2.imwrite('tmp/' + str(index) + '.png', thresh)
 
 def mse(im1, im2):
@@ -80,6 +114,7 @@ tEnd = time.time()
 
 print "Running Time : %f sec" % (tEnd - tStart)
 
-
+# cv2.imshow('image',im)
+# cv2.waitKey(0)
 
 #waitKey(0)
